a0de2bfd9b30948796782d1675d29a7aee5c4b4c
EnzeXu <xue20@wfu.edu>
2024-03-30 03:00:22 -0400

    update url parts

--- utils.py
+++ utils.py
@@ -336,10 +336,10 @@ def get_following_days(n=10, format="%m/%d/%Y"):
 def one_day_job(force=False):
     date_string = get_now_string("%m/%d/%Y")
     print(get_now_string(), f"[ ok ] Triggered on {date_string}.")
-    logging.info(f"Starting ... ")
     if not os.path.exists("./saves"):
         os.makedirs("./saves")
     logging.basicConfig(filename="./saves/logs.log", filemode="a", level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    logging.info(f"Starting ... ")
     # date_string_save = get_now_string("%Y-%m-%d")
     date_string_full = get_now_string()
     complete_cookie = get_cookie(f'https://williamandmary.campusdish.com/api/menu/GetMenus?locationId=78391&mode=Daily&date={date_string}')
